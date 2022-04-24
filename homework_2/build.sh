8794e13c24871ea1b57ff7bf9ce5aefb688daf45
Mario Namtao Shianti Larcher <mariolarcher1986@gmail.com>
2022-04-24 19:29:31 +0200

    Complete task 2

--- homework_2/build.sh
+++ homework_2/build.sh
@@ -7,3 +7,4 @@ cmake ..
 make
 cd ..
 bats tests/test_task_1.sh
+bats tests/test_task_2.sh
--- homework_2/cmake/ClangFormat.cmake
+++ homework_2/cmake/ClangFormat.cmake
@@ -6,12 +6,17 @@ endif()
 
 # Split the regex into multiple parts
 set(EXCLUDE_BUILD "-path ./build -prune -o")
+# -prune  It causes find to not descend into the current file.
 set(CPP_EXT "'.*\\.(cpp|cxx|cc|hpp|hxx|h)'")
 set(TRIM_OUT "-print |  tr '\\n' ';'")
+# -print  It prints the pathname of the current file to standard output.
 
 if (APPLE)
 set(REGEX "-regex")
+# -E      Interpret regular expressions followed by -regex and -iregex primaries as extended (modern) regular 
+#         expressions rather than basic regular expressions (BRE's).
 set(BASH_COMMAND "find -E . ${EXCLUDE_BUILD} ${REGEX} ${CPP_EXT} ${TRIM_OUT}")
+# find -- walk a file hierarchy
 else()
 set(REGEX "-regextype posix-extended -regex")
 set(BASH_COMMAND "find . ${EXCLUDE_BUILD} ${REGEX} ${CPP_EXT} ${TRIM_OUT}")
@@ -20,6 +25,7 @@ endif()
 # Get all project files
 execute_process(
   COMMAND bash -c "${BASH_COMMAND}"
+  # -c string If the -c option is present, then commands are read from string.
   OUTPUT_VARIABLE ALL_SOURCES
   OUTPUT_STRIP_TRAILING_WHITESPACE
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
